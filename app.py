80a83fb0ffe73c051483875385ebc19ca789409d
alpertucanberk <alpercanberk@Alpers-MacBook-Pro.local>
2020-03-09 20:21:09 -0700

    is this worth it?

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 #register blueprints for routes
 app.register_blueprint(routes)
 #initialize the excel module
