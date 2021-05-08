e19132677f07187e2f560da2ffd6e686b574412f
alpertucanberk <alper.tu.canberk@gmail.com>
2021-05-07 22:11:25 -0400

    aa

--- lists.py
+++ lists.py
@@ -1,12 +1,12 @@
 
 class Lists():
     announcements = [
-        {
-            "title":"Splash will begin on May 10th",
-            "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "date":"04/06/2021"
-
-        }
+        # {
+        #     "title":"Splash will begin on May 10th",
+        #     "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        #     "date":"04/06/2021"
+        #
+        # }
     ]
 
     rules = [
